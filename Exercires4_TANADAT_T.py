872c09e69a24ccd160eece32a17c31070e6f7657
Postmea <83636311+Postmea@users.noreply.github.com>
2021-05-04 16:23:49 +0700

    Create Exercires4_TANADAT_T.py

---
+++ Exercires4_TANADAT_T.py
@@ -0,0 +1,13 @@
+Subject1 = "Foundation English :"
+Subject2 = "General Business :"
+Subject3 = "Introduction to Computer Systems :"
+Subject4 = "Computer Programming :"
+ScoreS1 = 94.82
+ScoreS2 = 42
+ScoreS3 = 85
+ScoreS4 = 66.66
+print("------ Your Score ------")
+print(Subject1, ScoreS1)
+print(Subject2, ScoreS2)
+print(Subject3, ScoreS3)
+print(Subject4, ScoreS4)

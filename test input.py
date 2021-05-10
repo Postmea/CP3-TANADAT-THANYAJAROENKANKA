5232bc4ae12002d2a3ce0ca5336f0fc29766a77b
Postmea <83636311+Postmea@users.noreply.github.com>
2021-05-10 11:14:50 +0700

    Create test input.py

---
+++ test input.py
@@ -0,0 +1,7 @@
+name = input("First name :")
+lastname = input("Last name :")
+print("Hello",name,lastname)
+
+a = input("Number that you want : ")
+b = input("Number that you want : ")
+print("The number is",int(a)+int(b))

624775695f3e59c0930d6a0a1e4926dce1aa0ac5
Postmea <83636311+Postmea@users.noreply.github.com>
2021-05-10 11:46:35 +0700

    Create test log in.py

---
+++ test log in.py
@@ -0,0 +1,6 @@
+UsernameInput = input("Username :")
+PasswordInput = input("Password :")
+if UsernameInput == "admin" and PasswordInput == "1234" :
+    print("You are Authority")
+else:
+    print("Wrong Username or Password")

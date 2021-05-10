43454fa365602d9b2ebc8f66792357c672006d6f
Postmea <83636311+Postmea@users.noreply.github.com>
2021-05-10 12:05:24 +0700

    Create test score.py

---
+++ test score.py
@@ -0,0 +1,11 @@
+score = int(input("Your Score >>"))
+if score >= 80:
+    print("Your Grade is A")
+elif score >= 70:
+    print("Your Grade is B")
+elif score >= 60:
+    print("Your Grade is C")
+elif score >= 50:
+    print("Your Grade is D")
+else:
+    print("NOT PASS")

ae84b57156eaea391e570c82e7180aa917ca2bfe
Postmea <83636311+Postmea@users.noreply.github.com>
2021-08-02 21:16:53 +0700

    Create FunctionEx.py

---
+++ FunctionEx.py
@@ -0,0 +1,20 @@
+def SayName():
+    print("Post")
+def SayHello():
+    print("Hello")
+def Hi():
+    print("How are you today?")
+SayHello()
+Hi()
+SayName()
+
+def SayHelloWorld():
+    print("Hello World")
+    SayHello()
+    print("Woooooo !!")
+def SayHello():
+    print("Hello Post")
+    print("HaHa")
+    SayHelloWorld()
+SayHelloWorld()
+SayHello()

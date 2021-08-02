c2922a96236d99b2b0b12ca27230e00a0ef5f84a
Postmea <83636311+Postmea@users.noreply.github.com>
2021-08-02 20:51:11 +0700

    Update tip.py

--- tip.py
+++ tip.py
@@ -1,7 +1,15 @@
-##คำสั่ง print ให้คำติดกัน
-for i in range(10):
+##คำสั่ง print คำตอบจะอยู่แถวเดียวกันและติดกัน
+for i in range(3):
     print(i,end="")
 
 ##คำสั่ง print ธรรมดา คำตอบจะขึ้นทีละแถว
-for i in range(10):
+for i in range(3):
     print(i)
+
+##คำสั่ง print คำตอบจะอยู่แถวเดียวกันและห่างกัน 1 วรรค
+for i in range(3):
+    print(i,end=" ")
+
+##คำสั่ง print คำตอบจะอยู่แถวเดียวกันและมี -
+for i in range(3):
+    print(i, end="-")

b9cccda07bdc7c707f30a112c4c6bc2d284555a6
Postmea <83636311+Postmea@users.noreply.github.com>
2021-08-02 21:20:45 +0700

    Update FunctionParaEx..py

--- FunctionParaEx..py
+++ FunctionParaEx..py
@@ -1,3 +1,14 @@
-def addnumber(x,y):
+def addNumber(x,y):
     print(x+y)
-addnumber(10,89)
+def minusNumber(x,y):
+    print(x-y)
+def multipliedNumber(x,y):
+    print(x*y)
+def devideNumber(x,y):
+    print(x/y)
+x = int(input("value of x : "))
+y = int(input("value of y : "))
+addNumber(x,y)
+minusNumber(x,y)
+multipliedNumber(x,y)
+devideNumber(x,y)

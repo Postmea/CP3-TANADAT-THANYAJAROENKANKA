9536c17e0e5ab832248fbb2a6ba7a32d22018cf5
Postmea <83636311+Postmea@users.noreply.github.com>
2021-08-14 19:53:37 +0700

    Create WidgetEx4.py

---
+++ WidgetEx4.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+def leftClickButton(event):
+    print("Left Click !!")
+
+def rightClickButton(event):
+    print("Right Click !!")
+
+def doubleClickButton(event):
+    print("Double Click !!")
+
+main = Tk()
+button = Button(main,text = "My Button !!",activebackground = "blue")
+button.pack()
+button.bind('<Button-1>',leftClickButton)
+button.bind('<Button-3>',rightClickButton)
+button.bind('<Double-1>',doubleClickButton)
+main.mainloop()

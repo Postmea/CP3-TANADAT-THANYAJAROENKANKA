34a9c9455eed95a8fec0bf5daedafb0bd135b401
Postmea <83636311+Postmea@users.noreply.github.com>
2021-08-14 15:21:19 +0700

    Create TupleFunctionEx.py

---
+++ TupleFunctionEx.py
@@ -0,0 +1,9 @@
+tupleEx = (1,2,3,4,10)
+listEx = [1,2,3,4,11]
+print(len(tupleEx))
+print(len(listEx))
+print(max(tupleEx))
+print(tupleEx)
+print(listEx)
+print(tuple(listEx))
+print(tupleEx.index(1))

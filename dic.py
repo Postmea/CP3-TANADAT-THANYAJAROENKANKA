993d16f3fc28019bedf5ed5a534fbd0463847d56
Postmea <83636311+Postmea@users.noreply.github.com>
2021-08-14 15:21:32 +0700

    Create dic.py

---
+++ dic.py
@@ -0,0 +1,8 @@
+dict1 = {"Name":"Jame","Age":18}
+print(dict1["Name"],dict1["Age"])
+
+dict1["lastname"] = "Loof"
+print(dict1["Name"],dict1["lastname"],dict1["Age"])
+
+dict1["Name"] = "Jacop"
+print(dict1["Name"],dict1["lastname"],dict1["Age"])

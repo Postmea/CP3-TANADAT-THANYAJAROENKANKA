0fcc7dbb5f25680320f0b3a2a3707c999d79335e
Postmea <83636311+Postmea@users.noreply.github.com>
2021-08-14 10:45:24 +0700

    Create StringEx..py

---
+++ StringEx..py
@@ -0,0 +1,6 @@
+text = "Hello"
+print(text[0:3])  #เอาตัวเลขอักษรตัวที่ 1 (0) นับไป 3 ตัว (3)
+
+address = "642/17 Sukhumvit Bangkok"
+print("Bangkok" not in address)  #เป็นการค้นหาคำว่ามีหรือไม่มี เอาไปประยุกต์เป็นคำค้นได้
+
